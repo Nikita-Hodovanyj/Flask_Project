f3a6d7aee8b7054e3adeeef065233a10c1c4135e
Viktoria0228 <vikacold2010@gmail.com>
2024-10-13 17:07:26 +0300

    Tour page added

---
+++ main/urls.py
@@ -0,0 +1,5 @@
+import tour_page
+from .settings import project
+
+tour_page.tour.add_url_rule(rule = "/tour", view_func = tour_page.render_tour)
+project.register_blueprint(blueprint = tour_page.tour)
